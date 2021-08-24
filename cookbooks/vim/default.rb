8410c9c3fa6fcce24d4ba751430f62c44bdd8737
takehito <fukuda_kento@fukehito.com>
2021-08-24 21:58:46 +0000

    add installing vim plug

--- cookbooks/vim/default.rb
+++ cookbooks/vim/default.rb
@@ -1,1 +1,6 @@
 package 'silversearcher-ag'
+
+execute "install vim-plug" do
+  command "curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+  not_if "test -e ~/.vim/autoload/plug.vim"
+end
