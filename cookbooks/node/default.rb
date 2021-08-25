bd23c060271d9d0f29b992fb62c058152b822f84
takehito <fukuda_kento@fukehito.com>
2021-08-25 14:32:05 +0000

    add task to install node

--- cookbooks/node/default.rb
+++ cookbooks/node/default.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+execute 'curl -L https://nodejs.org/dist/v14.17.5/node-v14.17.5-linux-x64.tar.xz -o /tmp/node-v14.17.5-linux-x64.tar.xz'
+execute 'tar -xvf /tmp/node-v14.17.5-linux-x64.tar.xz -C /usr/local'
 execute 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -'
 
 execute 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list'
