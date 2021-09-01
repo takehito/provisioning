78b112a157c272c48c981297e9b4b96ed7ed8a7f
takehito <fukuda_kento@fukehito.com>
2021-09-01 19:26:43 +0000

    add cookbook to install dockle

---
+++ cookbooks/dockle/default.rb
@@ -0,0 +1,3 @@
+execute "curl -L -o /tmp/dockle.deb https://github.com/goodwithtech/dockle/releases/download/v0.3.16/dockle_0.3.16_Linux-64bit.deb"
+execute "dpkg -i /tmp/dockle.deb"
+execute "rm /tmp/dockle.deb"
